ad639176741483736a0d3eddb0344559038c2c54
ZS53 <zs53@telefonica.net>
2022-03-10 21:30:05 +0100

    add ejercicio frau

--- src/controllers/tipo.controller.js
+++ src/controllers/tipo.controller.js
@@ -1,160 +1,160 @@
-import Tipo from "../models/tipo.model";
-import { tiposMovimiento } from "../public/js/enumeraciones";
+import Tipo from '../models/tipo.model'
+import { tiposMovimiento } from '../public/js/enumeraciones'
 
-let tipo = new Tipo();
+let tipo = new Tipo()
 
 export const getTipos = async (req, res) => {
-  const { err, dat } = await tipo.getTipos();
+  const { err, dat } = await tipo.getTipos()
 
   if (err) {
-    return res.status(404).json({ err });
+    return res.status(404).json({ err })
   } else {
-    return res.status(200).json({ dat });
+    return res.status(200).json({ dat })
   }
-};
+}
 export const getTiposByOrigen = async (req, res) => {
-  tipo.origen = req.body.origen;
+  tipo.origen = req.body.origen
   try {
-    const { err, dat } = await tipo.getTiposByOrigen();
+    const { err, dat } = await tipo.getTiposByOrigen()
 
     if (err) {
-      res.status(402).json(err);
+      res.status(402).json(err)
     } else {
-      res.status(202).json(dat);
+      res.status(202).json(dat)
     }
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error)
   }
-};
+}
 export const getTipo = async (req, res) => {
-  tipo.id = req.body.id;
+  tipo.id = req.body.id
 
   try {
-    const { err, dat } = await tipo.getTipo();
+    const { err, dat } = await tipo.getTipo()
 
     if (err) {
-      res.status(403).json(err);
+      res.status(403).json(err)
     } else {
-      return res.status(202).json(tipo);
+      return res.status(202).json(tipo)
     }
   } catch (error) {
-    return res.status(404).json({ err });
+    return res.status(404).json({ err })
   }
-};
+}
 export const insertTipo = async (req, res) => {
-  const { usuarioMov, tipoMov } = req.body.movimiento;
+  const { usuarioMov, tipoMov } = req.body.movimiento
 
   // tipo
-  tipo.descripcion = req.body.tipo.destip;
-  tipo.textoAyuda = req.body.tipo.ayutip;
-  tipo.origen = req.body.tipo.orgtip;
+  tipo.descripcion = req.body.tipo.destip
+  tipo.textoAyuda = req.body.tipo.ayutip
+  tipo.origen = req.body.tipo.orgtip
   // movimiento
-  tipo.movimiento.usuario = usuarioMov;
-  tipo.movimiento.tipo = tipoMov;
+  tipo.movimiento.usuario = usuarioMov
+  tipo.movimiento.tipo = tipoMov
 
   try {
-    const { err, dat } = await tipo.insert();
+    const { err, dat } = await tipo.insert()
 
     if (err) {
-      res.status(404).json(err);
+      res.status(404).json(err)
     } else {
-      tipo.id = dat.p_idtipo;
+      tipo.id = dat.p_idtipo
 
-      res.status(202).json(tipo);
+      res.status(202).json(tipo)
     }
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error)
   }
-};
+}
 export const updateTipo = async (req, res) => {
-  const { usuarioMov, tipoMov } = req.body.movimiento;
+  const { usuarioMov, tipoMov } = req.body.movimiento
 
   try {
     // tipo
-    tipo.id = req.body.tipo.idtipo;
-    tipo.descripcion = req.body.tipo.destip;
-    tipo.textoAyuda = req.body.tipo.ayutip;
-    tipo.origen = req.body.tipo.orgtip;
+    tipo.id = req.body.tipo.idtipo
+    tipo.descripcion = req.body.tipo.destip
+    tipo.textoAyuda = req.body.tipo.ayutip
+    tipo.origen = req.body.tipo.orgtip
     // movimiento
-    tipo.movimiento.usuario = usuarioMov;
-    tipo.movimiento.tipo = tipoMov;
+    tipo.movimiento.usuario = usuarioMov
+    tipo.movimiento.tipo = tipoMov
 
-    const { err, dat } = await tipo.update();
+    const { err, dat } = await tipo.update()
 
     if (err) {
-      res.status(404).json(err);
+      res.status(404).json(err)
     } else {
-      res.status(202).json(tipo);
+      res.status(202).json(tipo)
     }
   } catch (error) {
-    res.status(405).json(error);
+    res.status(405).json(error)
   }
-};
+}
 export const deleteTipo = async (req, res) => {
-  const { usuarioMov, tipoMov } = req.body.movimiento;
+  const { usuarioMov, tipoMov } = req.body.movimiento
 
   try {
     // tipo
-    tipo.id = req.body.tipo.idtipo;
+    tipo.id = req.body.tipo.idtipo
     // movimiento
-    tipo.movimiento.usuario = usuarioMov;
-    tipo.movimiento.tipo = tipoMov;
+    tipo.movimiento.usuario = usuarioMov
+    tipo.movimiento.tipo = tipoMov
 
-    const { err, dat } = await tipo.delete();
+    const { err, dat } = await tipo.delete()
 
     if (err) {
-      res.status(404).json(err);
+      res.status(404).json(err)
     } else {
-      res.status(204).json(tipo);
+      res.status(204).json(tipo)
     }
   } catch (error) {
-    res.status(405).json(error);
+    res.status(405).json(error)
   }
-};
+}
 export const cambioPassword = async (req, res) => {
-  const { id, password } = req.body.usuario;
+  const { id, password } = req.body.usuario
 
   try {
-    const passSalt = await bcrypt.genSalt(10);
-    const passHash = await bcrypt.hash(password, passSalt);
+    const passSalt = await bcrypt.genSalt(10)
+    const passHash = await bcrypt.hash(password, passSalt)
 
-    usuario.id = id;
-    usuario.password = passHash;
+    usuario.id = id
+    usuario.password = passHash
     // movimiento
-    usuario.movimiento.usuario = id;
-    usuario.movimiento.tipo = tiposMovimiento.cambioPassword;
+    usuario.movimiento.usuario = id
+    usuario.movimiento.tipo = tiposMovimiento.cambioPassword
 
-    const { err, dat } = await usuario.cambioPassword();
+    const { err, dat } = await usuario.cambioPassword()
 
     if (err) {
-      res.status(404).json(err);
+      res.status(404).json(err)
     } else {
-      res.status(202).json(usuario);
+      res.status(202).json(usuario)
     }
   } catch (error) {
-    res.status(405).json(error);
+    res.status(405).json(error)
   }
-};
+}
 export const updatePerfil = async (req, res) => {
-  const { id, nombre, email, telefono } = req.body.usuario;
+  const { id, nombre, email, telefono } = req.body.usuario
 
   try {
-    usuario.id = id;
-    usuario.nombre = nombre;
-    usuario.email = email;
-    usuario.telefono = telefono;
+    usuario.id = id
+    usuario.nombre = nombre
+    usuario.email = email
+    usuario.telefono = telefono
     // movimiento
-    usuario.movimiento.usuario = id;
-    usuario.movimiento.tipo = tiposMovimiento.modificarUsuario;
+    usuario.movimiento.usuario = id
+    usuario.movimiento.tipo = tiposMovimiento.modificarUsuario
 
-    const { err, dat } = await usuario.updatePerfil();
+    const { err, dat } = await usuario.updatePerfil()
 
     if (err) {
-      res.status(404).json(err);
+      res.status(404).json(err)
     } else {
-      res.status(202).json(usuario);
+      res.status(202).json(usuario)
     }
   } catch (error) {
-    res.status(405).json(error);
+    res.status(405).json(error)
   }
-};
+}
