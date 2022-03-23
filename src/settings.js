98abeb495f829666352c805ca691d2b175aa3b42
zs153 <tmp153@gmail.com>
2022-03-23 09:42:48 +0100

    add method post a gentes

--- src/settings.js
+++ src/settings.js
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
-
 export const connectionString = {
   user: process.env.NODE_ORACLEDB_USER,
   password: process.env.NODE_ORACLEDB_PASSWORD,
