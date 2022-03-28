a894653883a6bfed5029ecdcd98500d98686be52
zs153 <tmp153@gmail.com>
2022-03-28 10:12:41 +0200

    limpieza console.log

--- src/controllers/gente.controller.js
+++ src/controllers/gente.controller.js
@@ -1,30 +1,29 @@
-import Gente from '../models/gente.model'
+import Gente from "../models/gente.model";
 
-let gente = new Gente()
+let gente = new Gente();
 
 export const getGente = async (req, res) => {
-  const nif = req.body.nifgen
+  const nif = req.body.nifgen;
 
   if (nif.length === 9) {
-    gente.nif = nif
+    gente.nif = nif;
   } else {
-    gente.nif = nif.slice(0, 9)
-    gente.discrim = nif.slice(-1)
+    gente.nif = nif.slice(0, 9);
+    gente.discrim = nif.slice(-1);
   }
 
   try {
     const { err, dat } =
       nif.length === 9
         ? await gente.getGenteByNif()
-        : await gente.getGenteByNifDiscriminante()
+        : await gente.getGenteByNifDiscriminante();
 
     if (err) {
-      res.status(403).json(err)
+      res.status(403).json(err);
     } else {
-      return res.status(200).json(dat)
+      return res.status(200).json(dat);
     }
   } catch (error) {
-    console.log(error)
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
-}
+};
