fd4e442eb0d69f97514c854c57095e1e46eab952
ZS53 <zs53@telefonica.net>
2022-12-01 17:34:02 +0100

    upd

--- src/controllers/estadistica.controller.js
+++ src/controllers/estadistica.controller.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { tiposMovimiento } from '../public/js/enumeraciones'
 
 // pages
 export const mainPage = async (req, res) => {
@@ -7,20 +8,18 @@ export const mainPage = async (req, res) => {
   const currentYear = fecha.getFullYear()
   const currentMonth = fecha.getMonth() + 1
   const lastDayMonth = new Date(currentYear, currentMonth, 0).getDate()
-  
+
   let desde = new Date(yearMonthDayToUTCString(currentYear, currentMonth, 1)).toISOString().slice(0, 10)
   let hasta = new Date(yearMonthDayToUTCString(currentYear, currentMonth, lastDayMonth)).toISOString().slice(0, 10)
- 
+
   try {
     const cargas = await axios.post('http://localhost:8000/api/cargas', {})
-    const tipos = await axios.post('http://localhost:8000/api/tipos', {})
     const datos = {
       desde,
       hasta,
       cargas: cargas.data,
-      tipos: tipos.data,
     }
-    console.log(datos.desde, datos.hasta)
+
     res.render('admin/estadisticas', { user, datos })
   } catch (error) {
     const msg = 'No se ha podido acceder a los datos de la aplicación.'
@@ -39,58 +38,56 @@ export const generarEstadistica = async (req, res) => {
     HASTA: req.body.hasta,
   }
   const formulario = {
-    REFDOC: req.bodu.refcar,
+    REFDOC: req.body.refdoc,
   }
   const carga = {
-    refcar: req.body.refcar,
+    REFCAR: req.body.refdoc,
   }
 
   try {
-    const situacion = await axios.post('http://localhost:8100/api/estadisticas/situacion', {
-      periodo,
+    const situacion = await axios.post('http://localhost:8000/api/estadisticas/situacion', {
       formulario,
     })
-    const oficinas = await axios.post('http://localhost:8100/api/estadisticas/oficinas', {
+    const oficinas = await axios.post('http://localhost:8000/api/estadisticas/oficinas', {
       carga,
     })
-    const actuacion = await axios.post('http://localhost:8100/api/estadisticas/actuacion', {
+    const actuacion = await axios.post('http://localhost:8000/api/estadisticas/actuacion', {
+      formulario,
+      tiposMovimiento,
       periodo,
     })
 
-    const serieL = []
-    const serieS = []
-    const serieC = []
+    const serieA = []
+    const serieR = []
 
     actuacion.data.map(itm => {
-      serieC.push({ x: itm.FEC, y: itm.COR })
-      serieL.push({ x: itm.FEC, y: itm.LIQ })
-      serieS.push({ x: itm.FEC, y: itm.SAN })
+      serieA.push({ x: itm.FEC, y: itm.ASI })
+      serieR.push({ x: itm.FEC, y: itm.RES })
     })
 
-    const totalSituacion = situacion.data.TOTAL
+    const totalSituacion = situacion.data.TOT
     const ratios = {
-      // propuestaLiquidacion: Math.round((hitos.data.PROLIQ * 100 / totalSituacion) * 100) / 100.0,
-      // propuestaSancion: Math.round((hitos.data.PROSAN * 100 / totalSituacion) * 100) / 100.0,
-      correctas: Math.round((situacion.data.CORREC * 100 / totalSituacion) * 100) / 100.0,
-      liquidacion: Math.round((hitos.data.LIQUID * 100 / totalSituacion) * 100) / 100.0,
-      sancion: Math.round((hitos.data.SANCIO * 100 / totalSituacion) * 100) / 100.0,
-      // anulacion: Math.round((hitos.data.ANUSAN * 100 / totalSituacion) * 100) / 100.0,
+      pendientes: Math.round((situacion.data.PEN * 100 / totalSituacion) * 100) / 100.0,
+      asignadas: Math.round((situacion.data.ASI * 100 / totalSituacion) * 100) / 100.0,
+      resueltas: Math.round((situacion.data.RES * 100 / totalSituacion) * 100) / 100.0,
     }
 
     const datos = {
-      fraude,
-      periodo: { desde: new Date(periodo.desde).toLocaleDateString('es-ES', { year: 'numeric', month: '2-digit', day: '2-digit' }), hasta: new Date(periodo.hasta).toLocaleDateString('es-ES', { year: 'numeric', month: '2-digit', day: '2-digit' }) },
-      tipo,
-      hitos: hitos.data,
+      carga,
+      strPeriodo: {
+        desde: new Date(periodo.DESDE).toLocaleDateString('es-ES', { year: 'numeric', month: '2-digit', day: '2-digit' }),
+        hasta: new Date(periodo.HASTA).toLocaleDateString('es-ES', { year: 'numeric', month: '2-digit', day: '2-digit' }),
+      },
+      periodo,
+      actuacion: actuacion.data,
       oficinas: oficinas.data,
       situacion: situacion.data,
       ratios,
-      serieC: JSON.stringify(serieC),
-      serieL: JSON.stringify(serieL),
-      serieS: JSON.stringify(serieS),
+      serieA: JSON.stringify(serieA),
+      serieR: JSON.stringify(serieR),
     }
 
-    res.render('admin/estadisticas/fraudes', { user, datos })
+    res.render('admin/estadisticas/resultado', { user, datos })
   } catch (error) {
     const msg = 'No se ha podido acceder a los datos de la aplicación.'
 
@@ -101,22 +98,6 @@ export const generarEstadistica = async (req, res) => {
 }
 
 // helpers
-const formatDate = (day,month,year) => {
-  return [
-    padTo2Digits(day),
-    padTo2Digits(month),
-    year
-  ].join('/')
-}
-const padTo2Digits = (num) => {
-  return num.toString().padStart(2, '0');
-}
-const dateISOToUTCString = (date) => {
-  const fecha = new Date(date)
-  const userTimezoneOffset = fecha.getTimezoneOffset() * 60000
-
-  return new Date(fecha.getTime() - userTimezoneOffset)
-}
 const yearMonthDayToUTCString = (year, month, day) => {
   const yearCDM = ('000' + year).slice(-4)
   const monthCDM = ('0' + month).slice(-2)
